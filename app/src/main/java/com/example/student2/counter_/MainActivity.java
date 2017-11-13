bdd56fceef87d87c6c596eb7e7253310321ec4b4
SokolDZR <antonyuk-daniil@mail.ru>
2017-11-13 19:47:33 +0300

    +-*/

--- app/src/main/java/com/example/student2/counter_/MainActivity.java
+++ app/src/main/java/com/example/student2/counter_/MainActivity.java
@@ -23,7 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void minus(View v){
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String snum1 = num1.getText().toString();
         EditText num2 = (EditText) this.findViewById(R.id.num2);
         String snum2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText("" + (Double.parseDouble(snum1) - Double.parseDouble(snum2)));
+    }
+    public void multiplication(View v){
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String snum1 = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String snum2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText("" + (Double.parseDouble(snum1)*Double.parseDouble(snum2)));
+    }
+    public void division(View v){
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String snum1 = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String snum2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText("" + (Double.parseDouble(snum1)/Double.parseDouble(snum2)));
     }
 }
