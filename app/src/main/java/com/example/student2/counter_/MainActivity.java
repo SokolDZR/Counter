627b520e835d55469f45fd8730d6a68587f3e809
SokolDZR <antonyuk-daniil@mail.ru>
2017-11-13 19:38:03 +0300

    +-

--- app/src/main/java/com/example/student2/counter_/MainActivity.java
+++ app/src/main/java/com/example/student2/counter_/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.student2.counter_;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void plus(View v){
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String snum1 = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String snum2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText("" + (Double.parseDouble(snum1) + Double.parseDouble(snum2)));
+
+    }
+    public void minus(View v){
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String snum2 = num2.getText().toString();
     }
 }
