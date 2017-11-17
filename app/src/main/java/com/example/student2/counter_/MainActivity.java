7ebba37885cd7c7dc3f9f868552b9f24434f696c
SokolDZR <antonyuk-daniil@mail.ru>
2017-11-17 19:45:52 +0300

    17.11.17

--- app/src/main/java/com/example/student2/counter_/MainActivity.java
+++ app/src/main/java/com/example/student2/counter_/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
     public void plus(View v){
         EditText num1 = (EditText) this.findViewById(R.id.num1);
